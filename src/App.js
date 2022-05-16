d78f507764f7fc42cdcabb87c4f0f243aa272e14
Hendrik <hawc@users.noreply.github.com>
2022-05-16 16:31:46 +0900

    added extend method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import React, { Suspense, useRef, useState } from 'react';
-import { Canvas, useFrame, useResource } from 'react-three-fiber';
+import { extend, Canvas, useFrame, useResource } from 'react-three-fiber';
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from 'react-postprocessing';
 import { Html, Icosahedron, useTextureLoader, useCubeTextureLoader, MeshDistortMaterial } from 'drei';
 import { Text } from 'troika-three-text';
