e09d148951d6b9c446fd69c7280bf680e3f7b13d
Hendrik <hawc@users.noreply.github.com>
2022-05-17 00:27:25 +0900

    test

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ export default function App() {
       <color attach="background" args={['#050505']} />
       <fog color="#161616" attach="fog" near={8} far={30} />
       <Suspense fallback={<Html center>Loading.</Html>}>
-        <Preload all>
+        <Preload all />
         <Scene/>
         <SceneText />
         <EffectComposer>
