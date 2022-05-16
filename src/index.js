33433c0e834d92b7ebc11af842f04f822f0785cb
Hendrik <hawc@users.noreply.github.com>
2022-05-16 21:43:13 +0900

    test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
+import {preloadFont} from 'troika-three-text'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const opts = {
+  font: "Roboto Slab",
+  fontSize: 6,
+  color: "#010101",
+  maxWidth: 300,
+};
+
+preloadFont(
+  {
+    font: fonts[opts.font],
+    characters: 'abcdefghijklmnopqrstuvwxyz'
+  },
+  () => {
+    ReactDOM.render(<App />, document.getElementById('root'))
+  }
+)
+
