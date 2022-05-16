c1f904456a135bc197c89bfed773a406524a4510
Hendrik <hawc@users.noreply.github.com>
2022-05-16 20:08:36 +0900

    test

--- src/App.js
+++ src/App.js
@@ -110,7 +110,9 @@ function Scene() {
 
 export default function App() {
   return (
-    <Canvas>
+    <Canvas
+      camera={{ position: [0, 0, 3] }}
+      gl={{ powerPreference: 'high-performance', alpha: false, antialias: false, stencil: false, depth: false }}>
       <color attach="background" args={['#050505']} />
       <fog color="#161616" attach="fog" near={8} far={30} />
       <Suspense fallback={<Html center>Loading.</Html>}>
