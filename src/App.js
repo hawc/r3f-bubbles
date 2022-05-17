17ca5028174761d676d6a670cad21e09fec9db1e
Hendrik <hawc@users.noreply.github.com>
2022-05-17 17:31:55 +0900

    test

--- src/App.js
+++ src/App.js
@@ -59,10 +59,10 @@ export default function App() {
       <Suspense fallback={<Html center>Loading.</Html>}>
         <SceneText />
         <EffectComposer multisampling={0}>
-          <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+          {/*<DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
           <Bloom luminanceSmoothing={0.1} luminanceThreshold={0.2} />
           <Noise opacity={0.03} />
-          <Vignette darkness={0.5} />
+          <Vignette darkness={0.5} />*/}
           <Pixelation />
           <BrightnessContrast contrast={1} />
         </EffectComposer>
