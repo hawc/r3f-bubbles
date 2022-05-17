b6798f02225cda9336c1c2a2783a2b0b26986878
Hendrik <hawc@users.noreply.github.com>
2022-05-17 14:58:34 +0900

    test

--- src/App.js
+++ src/App.js
@@ -25,47 +25,6 @@ function MainSphere({ material }) {
   return <Icosahedron args={[1, 4]} ref={main} material={material} position={[0, 0, 0]} />
 }
 
-function Instances({ material }) {
-  // we use this array ref to store the spheres after creating them
-  const [sphereRefs] = useState(() => [])
-  // we use this array to initialize the background spheres
-  const initialPositions = [
-    [-4, 20, -12],
-    [-10, 12, -4],
-    [-11, -12, -23],
-    [-16, -6, -10],
-    [12, -2, -3],
-    [13, 4, -12],
-    [14, -2, -23],
-    [8, 10, -20],
-  ]
-  // smaller spheres movement
-  useFrame(() => {
-    // animate each sphere in the array
-    sphereRefs.forEach((el) => {
-      el.position.y += 0.02
-      if (el.position.y > 19) el.position.y = -18
-      el.rotation.x += 0.06
-      el.rotation.y += 0.06
-      el.rotation.z += 0.02
-    })
-  })
-  return (
-    <>
-      <MainSphere material={material} />
-      {initialPositions.map((pos, i) => (
-        <Icosahedron
-          args={[1, 4]}
-          position={[pos[0], pos[1], pos[2]]}
-          material={material}
-          key={i}
-          ref={(ref) => (sphereRefs[i] = ref)}
-        />
-      ))}
-    </>
-  )
-}
-
 function Scene() {
   const bumpMap = useTextureLoader('/bump.jpg')
   const envMap = useCubeTextureLoader(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'], { path: '/cube/' })
@@ -86,7 +45,7 @@ function Scene() {
         envMap={envMap}
         bumpMap={bumpMap}
       />
-      {material && <Instances material={material} />}
+      {material && <MainSphere material={material} />}
     </>
   )
 }
@@ -122,6 +81,7 @@ function SceneText() {
       >
         wieauchimmer
       </Text>
+      {material && <MainSphere material={material} />}
     </>
   )
 }
